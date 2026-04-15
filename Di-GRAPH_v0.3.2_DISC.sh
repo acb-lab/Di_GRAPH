e87c655b18e108b8abacd9598f011791928f06b9
lydiaiglesias <lydiaiglesias@usal.es>
2026-04-15 16:42:47 +0200

    last version

--- Di-GRAPH_v0.3.2_DISC.sh
+++ Di-GRAPH_v0.3.2_DISC.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 AUTHORS="Lydia Iglesias, Narciso M. Quijada, Andrés Clemente-Blanco"
-LASTMODIF="2026-04-11"
+LASTMODIF="2026-04-15"
 VERSION="0.3.2"
 MODIFICATIONS="experiment name dynamically set, MUT"
 
